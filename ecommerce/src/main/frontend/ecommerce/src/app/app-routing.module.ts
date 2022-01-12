c2370ad0ccc382c3623e6cadd0dde27f06d56ab8
jekanest <jekanest@yahoo.com>
2022-01-13 00:58:05 +0200

    nav menu routing pages created

--- ecommerce/src/main/frontend/ecommerce/src/app/app-routing.module.ts
+++ ecommerce/src/main/frontend/ecommerce/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { UsersComponent } from './ecommerce/admin/users/users.component';
 import { AboutComponent } from './ecommerce/about/about.component';
+import { ProductsComponent } from './ecommerce/products/products.component';
+import { ProductDetailsComponent } from './ecommerce/product-details/product-details.component';
+import { EcommerceComponent } from './ecommerce/ecommerce.component';
+import { ShoppingCartComponent } from './ecommerce/shopping-cart/shopping-cart.component';
 
 const routes: Routes = [
+  { path: 'home', component: EcommerceComponent },
+  { path: 'yewelery', component: ProductsComponent},
   { path: 'about', component: AboutComponent },
+  { path: 'details', component: ProductDetailsComponent },
+ /* { path: 'faq', component: FaqComponent },
+  { path: 'policies', component: PoliciesComponent },
+  { path: 'login', component: LoginComponent },*/
 ];
 
 @NgModule({
