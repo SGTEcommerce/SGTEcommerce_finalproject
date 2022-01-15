d1e315b753559821eb8e4ffdc46bc7281d5f93ea
ZannaJ <zhanna.jelatanceva@gmail.com>
2022-01-15 19:11:27 +0200

    new branch

---
+++ ecommerce/src/main/java/lv/sgtecommerce/finalproject/services/UserService.java
@@ -0,0 +1,10 @@
+package lv.sgtecommerce.finalproject.services;
+
+import lv.sgtecommerce.finalproject.models.User;
+
+public interface UserService {
+
+    void save(User user);
+    User findByUsername(String username);
+
+}
