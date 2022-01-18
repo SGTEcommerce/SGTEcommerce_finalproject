dc21485cd49845267ec318dbd337bcd9918a9cd6
jekanest <jekanest@yahoo.com>
2022-01-18 23:20:44 +0200

    login second try

--- ecommerce/src/main/frontend/ecommerce/src/app/ecommerce/login/login.component.ts
+++ ecommerce/src/main/frontend/ecommerce/src/app/ecommerce/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UsersService } from '../admin/users/users.service';
+import { Token } from '../models/Token';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  public username : string
+  public password : string
+  public error : string
 
-  constructor() { }
+  constructor(private usersService : UsersService, private router : Router) { }
 
   ngOnInit(): void {
-  }
+    if (localStorage.getItem('token')) {
+        this.router.navigateByUrl('/account')
+    }
+}
 
+logIn () {
+    this.error = ''
+
+    this.usersService.login(
+        this.username, this.password).subscribe((token : Token) => {
+            localStorage.setItem('token', token.token);
+            this.router.navigateByUrl('/account').then(() => window.location.reload())
+        }, (error : ErrorEvent) => {
+            console.log(error);
+            this.error = "Invalid login credentials"
+        })
+    
+}
 }
--- ecommerce/src/main/java/lv/sgtecommerce/finalproject/models/User.java
+++ ecommerce/src/main/java/lv/sgtecommerce/finalproject/models/User.java
@@ -21,6 +21,15 @@ public class User {
     @NotEmpty
     private String username;
 
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, length = 128)
+    private String address;
+
+    @Column(nullable = false, length = 15)
+    private String phone;
+
     @Column(name = "password")
     @NotEmpty
     @Size(min = 5, message = "Length must be more than 5")
@@ -31,6 +40,18 @@ public class User {
 
     @ManyToOne
     private Role role;
+
+    public User(Long id, String email, String username, String name, String address, String phone, String password, String passwordConfirm, Role role) {
+        this.id = id;
+        this.email = email;
+        this.username = username;
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.password = password;
+        this.passwordConfirm = passwordConfirm;
+        this.role = role;
+    }
 
     public Long getId() {
         return id;
@@ -79,15 +100,41 @@ public class User {
     public void setRole(Role role) {
         this.role = role;
     }
+    public String getName() {
+        return name;
+    }
 
-    /*@Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
     public String toString() {
         return "User{" +
                 "id=" + id +
                 ", email='" + email + '\'' +
                 ", username='" + username + '\'' +
                 ", password='" + password + '\'' +
+                 ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", phone='" + phone + '\'' +
                 ", role='" + role + '\'' +
                 '}';
-    }*/
+    }
 }
